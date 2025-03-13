4c118b2037abe8f1f78ae967fb0b971d464a0f4c
Hetvi <hetukathiriya22@gmail.com>
2025-03-13 11:09:47 +0530

    create home page file and images

---
+++ recipe/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+        <section className='home'>
+            <div className='left'>
+                <h1>Food Recipe</h1>
+                <h5>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</h5>
+                <button>Share your recipe</button>
+            </div>
+            <div className='right'>
+                <img src="" alt="" />
+            </div>
+        </section>
+    </>
+  )
+}
