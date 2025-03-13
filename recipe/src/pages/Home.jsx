da1871e412277a8ffd9b8d96b162c4889befdac2
Hetvi <hetukathiriya22@gmail.com>
2025-03-13 11:19:37 +0530

    add bg in home page

--- recipe/src/pages/Home.jsx
+++ recipe/src/pages/Home.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
+import foodRecipe from "../assets/foodRecipe.png";
 
 export default function Home() {
   return (
     <>
-        <section className='home'>
-            <div className='left'>
-                <h1>Food Recipe</h1>
-                <h5>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</h5>
-                <button>Share your recipe</button>
-            </div>
-            <div className='right'>
-                <img src="" alt="" />
-            </div>
-        </section>
+      <section className="home">
+        <div className="left">
+          <h1>Food Recipe</h1>
+          <h5>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable
+          </h5>
+          <button>Share your recipe</button>
+        </div>
+        <div className="right">
+          <img src={foodRecipe} width="320px" height="300px"></img>
+        </div>
+      </section>
+      <div className="bg">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#d4f6e8"
+            fillOpacity="1"
+            d="M0,32L40,32C80,32,160,32,240,58.7C320,85,400,139,480,149.3C560,160,640,128,720,101.3C800,75,880,53,960,80C1040,107,1120,181,1200,213.3C1280,245,1360,235,1400,229.3L1440,224L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
     </>
-  )
+  );
 }
