9ffb22aaf090681413ad01188048200a67a06794
Hetvi <hetukathiriya22@gmail.com>
2025-03-19 21:58:24 +0530

    add recipe and recipe add in loop

--- recipe/src/components/Recipeitems.jsx
+++ recipe/src/components/Recipeitems.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import foodImg from "../assets/foodRecipe.png";
+import { BsFillStopwatchFill } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa";
 
 export default function Recipeitems() {
+  const allRecipes = useLoaderData();
+  console.log(allRecipes);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="card-container">
+        {allRecipes?.map((item, index) => {
+          return (
+            <div key={index} className="card">
+              <img src={foodImg} width="120px" height="100px"></img>
+              <div className="card-body">
+                <div className="title">{item.title}</div>
+                <div className="icons">
+                  <div className="timer">
+                    <BsFillStopwatchFill />
+                  </div>
+                  <FaHeart />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
