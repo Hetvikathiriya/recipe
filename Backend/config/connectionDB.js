352288008386339ca4f2071dfc3c90bf81f60093
Hetvi <hetukathiriya22@gmail.com>
2025-03-19 11:32:45 +0530

    connect database

--- Backend/config/connectionDB.js
+++ Backend/config/connectionDB.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose")
 
 const connectDB=async()=>{
-    await mongoose.connect()
+    await mongoose.connect(process.env.CONNECTION_STRING).then(()=>console.log("connected..."))
 }
+
+module.exports=connectDB
