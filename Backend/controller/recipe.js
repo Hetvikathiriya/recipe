d478929d66b80ae9140d11cf0932e356e4ff7bdc
Hetvi <hetukathiriya22@gmail.com>
2025-03-19 15:34:44 +0530

    add get all recipes

--- Backend/controller/recipe.js
+++ Backend/controller/recipe.js
@@ -1,37 +1,54 @@
 const Recipes=require("../models/recipe")
 
-const getRecipes=(req,res)=>{
-    res.json({message:"hello"})
+// get all recipes
+const getRecipes=async(req,res)=>{
+    const recipes=await Recipes.find()
+    return res.json(recipes)
 }
 
-const getRecipe=  (req,res)=>{
-    res.json({message:"hello"})
+// get single recipe by id
+const getRecipe=async(req,res)=>{
+    const recipe=await Recipes.findById(req.params.id)
+    res.json(recipe)
 }
 
 // add recipe 
 const addRecipe=async (req,res)=>{
      const {title, ingredients, instructions, time}=req.body //Extracting Data from Request
 
-
     //  check condition of required field
      if(!title || !ingredients || !instructions)
      {
         res.json({message: "Required fields can't be emplty"})
      }
-
     //  store recipe in database
      const newRecipe=await Recipes.create({
         title, ingredients, instructions, time
     })
-
     // return response
     return res.json(newRecipe)
 }
 
-const  editRecipe=(req,res)=>{
-    res.json({message:"hello"})
+// edit recipe
+const  editRecipe=async(req,res)=>{
+    const {title, ingredients, instructions, time}=req.body //Extracting Data from Request
+
+    // check recipe is already present or not
+    let recipe=await Recipes.findById(req.params.id)
+
+    // handle error
+    try{
+        if(recipe){
+            await Recipes.findByIdAndUpdate(req.params.id,req.body,{new:true})
+            res.json({title,ingredients,instructions,time})
+        }
+    }
+    catch(err){
+            return res.status(404).json({message:"error"})
+    } 
 }
 
+// delete recipe
 const  deleteRecipe=(req,res)=>{
     res.json({message:"hello"})
 }
